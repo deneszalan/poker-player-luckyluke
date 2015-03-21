9fb4562dc63d78737e21c7f4d79997f7fbc1d028
salacz <salacz.agoton@prolan.hu>
2015-03-21 13:02:55 +0100

    Ranking

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+
+CARDS = ['2', '3', '4', '5', '6', '7', '8', '9', 'J', 'D', 'K', 'A']
 
 class Player:
     VERSION = "All-in 1"
@@ -12,9 +13,60 @@ class Player:
     def isPair(self, cards):
         cards[0]["rank"]==cards[1]["rank"]
 
+    def getCardSorts(self, allcards):
+        ranks={}
+        suits={}
+        for card in allcards:
+            if not ranks.has_key(card["rank"]):
+                ranks[card["rank"]]=1
+            else:
+                ranks[card["rank"]]+=1
+            if card["suit"] not in suits:
+                suits[card["suit"]]=1
+            else:
+                suits[card["suit"]]+=1
+
+        print allcards
+        print ranks
+        print suits
+        return (ranks, suits)
+
+    def hasPair(self, ranks):
+        return 2 in ranks.values()
+
+    def twoPairs(self, ranks):
+        counts=ranks.values()
+        return 2 == counts.count(2)
+
+    def hasDrill(self, ranks):
+        return 3 in ranks.values()
+
+    def hasPoker(self, ranks):
+        return 4 in ranks.values()
+
+    def hasFull(self, ranks):
+        return (2 in ranks.values()) and (3 in ranks.values())
+
+    def hasFlush(self, suits):
+        return 5 in suits.values() or 6 in suits.values() or 7 in suits.values()
+
+    def hasStraight(self, cards):
+        for j in range(len(cards)-4):
+            i = CARDS.index(cards[j]["rank"])
+            straight = CARDS[i:i+5]
+            if cards[j:j+5] == straight:
+                return True
+        return False
+
+    def getRank(self, allcards, ranks, suits):
+        if len(allcards)!=5:
+            return 0
+
+        return 0
+
     def betRequest(self, game_state):
         # print "Hello"
-
+       
         me = game_state["players"][game_state["in_action"]]
         mycards = me["hole_cards"]
 
--- test.py
+++ test.py
@@ -66,5 +66,151 @@ def test1():
 
     p.betRequest(gameState)
 
+def testPairs():
+    cards = [                            # Finally the array of community cards.
+                                         {
+                                             "rank": "6",                    # Rank of the card. Possible values are numbers 2-10 and J,Q,K,A
+                                             "suit": "hearts"                # Suit of the card. Possible values are: clubs,spades,hearts,diamonds
+                                         },
+                                         {
+                                             "rank": "K",
+                                             "suit": "spades"
+                                         },
+                                         {
+                                             "rank": "A",
+                                             "suit": "spades"
+                                         },
+                                         {
+                                             "rank": "A",
+                                             "suit": "hearts"
+                                         },
+                                         {
+                                             "rank": "6",
+                                             "suit": "clubs"
+                                         }
+    ]
+    p=Player()
+    ranks, suits = p.getCardSorts(cards)
+    print "Pair:", p.hasPair(ranks)
+    print "Two pairs:", p.twoPairs(ranks)
+    print "Drill:", p.hasDrill(ranks)
+    print "Full:", p.hasFull(ranks)
+    print "Poker:", p.hasPoker(ranks)
+
+def testDrill():
+    cards = [                            # Finally the array of community cards.
+                                         {
+                                             "rank": "6",                    # Rank of the card. Possible values are numbers 2-10 and J,Q,K,A
+                                             "suit": "hearts"                # Suit of the card. Possible values are: clubs,spades,hearts,diamonds
+                                         },
+                                         {
+                                             "rank": "A",
+                                             "suit": "spades"
+                                         },
+                                         {
+                                             "rank": "6",
+                                             "suit": "spades"
+                                         },
+                                         {
+                                             "rank": "A",
+                                             "suit": "hearts"
+                                         },
+                                         {
+                                             "rank": "6",
+                                             "suit": "clubs"
+                                         }
+    ]
+    p=Player()
+    ranks, suits = p.getCardSorts(cards)
+    print "Pair:", p.hasPair(ranks)
+    print "Two pairs:", p.twoPairs(ranks)
+    print "Drill:", p.hasDrill(ranks)
+    print "Full:", p.hasFull(ranks)
+    print "Poker:", p.hasPoker(ranks)
+
+def testPoker():
+    cards = [                            # Finally the array of community cards.
+                                         {
+                                             "rank": "A",                    # Rank of the card. Possible values are numbers 2-10 and J,Q,K,A
+                                             "suit": "diamonds"              # Suit of the card. Possible values are: clubs,spades,hearts,diamonds
+                                         },
+                                         {
+                                             "rank": "A",
+                                             "suit": "spades"
+                                         },
+                                         {
+                                             "rank": "6",
+                                             "suit": "spades"
+                                         },
+                                         {
+                                             "rank": "A",
+                                             "suit": "hearts"
+                                         },
+                                         {
+                                             "rank": "A",
+                                             "suit": "clubs"
+                                         }
+    ]
+    p=Player()
+    ranks, suits = p.getCardSorts(cards)
+    print "Pair:", p.hasPair(ranks)
+    print "Two pairs:", p.twoPairs(ranks)
+    print "Drill:", p.hasDrill(ranks)
+    print "Full:", p.hasFull(ranks)
+    print "Poker:", p.hasPoker(ranks)
+
+def testStraight():
+    cards1 = [                            # Finally the array of community cards.
+                                         {
+                                             "rank": "5",                    # Rank of the card. Possible values are numbers 2-10 and J,Q,K,A
+                                             "suit": "diamonds"              # Suit of the card. Possible values are: clubs,spades,hearts,diamonds
+                                         },
+                                         {
+                                             "rank": "6",
+                                             "suit": "spades"
+                                         },
+                                         {
+                                             "rank": "7",
+                                             "suit": "spades"
+                                         },
+                                         {
+                                             "rank": "8",
+                                             "suit": "hearts"
+                                         },
+                                         {
+                                             "rank": "J",
+                                             "suit": "clubs"
+                                         }
+    ]
+    cards2 = [                            # Finally the array of community cards.
+                                         {
+                                             "rank": "8",                    # Rank of the card. Possible values are numbers 2-10 and J,Q,K,A
+                                             "suit": "diamonds"              # Suit of the card. Possible values are: clubs,spades,hearts,diamonds
+                                         },
+                                         {
+                                             "rank": "9",
+                                             "suit": "spades"
+                                         },
+                                         {
+                                             "rank": "J",
+                                             "suit": "spades"
+                                         },
+                                         {
+                                             "rank": "D",
+                                             "suit": "hearts"
+                                         },
+                                         {
+                                             "rank": "K",
+                                             "suit": "clubs"
+                                         }
+    ]
+    p=Player()
+    print "Straight:", p.hasStraight(cards1)
+    print "Straight:", p.hasStraight(cards2)
+
 if __name__ == '__main__':
     test1()
+    testPairs()
+    testDrill()
+    testPoker()
+    testStraight()
